34c9d8788c75418a8252d1d8cc8e3c4604a0eab9
somchoonn <chanokchon.pancome@gmail.com>
2025-10-29 03:15:19 +0700

    Part 1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ipa-project
 IPA-Project
+
+# === MySQL ===
+MYSQL_ROOT_PASSWORD=rootpassword
+MYSQL_DATABASE=ipa2025
+MYSQL_USER=root
+MYSQL_PASSWORD=rootpassword
+
+# === Flask ===
+DB_HOST=mysql
+DB_USER=root
+DB_PASSWORD=rootpassword
+DB_NAME=ipa2025
+
+# === Ports ===
+WEB_PORT=8080
+MYSQL_PORT=3306
+
+# == Command ==
+docker compose up --build --detach
+docker compose down
---
+++ web/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+import os
+import time
+
+app = Flask(__name__)
+
+
+time.sleep(10)
+
+# Connect DB
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME",)
+)
+cursor = db.cursor(dictionary=True)
+
+# Main
+@app.route("/", methods=["GET"])
+def index():
+    cursor.execute("SELECT * FROM routers")
+    routers = cursor.fetchall()
+    return render_template("index.html", routers=routers)
+
+# Add Router
+@app.route("/add", methods=["POST"])
+def add_router():
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if ip and username and password:
+        cursor.execute("""
+            INSERT INTO routers (ip, username, password)
+            VALUES (%s, %s, %s)
+        """, (ip, username, password))
+        db.commit()
+    return redirect("/")
+
+# Delete Router
+@app.route("/delete/<int:ip>", methods=["POST"])
+def delete_router(ip):
+    cursor.execute("DELETE FROM routers WHERE ip = %s", (ip,))
+    db.commit()
+    return redirect("/")
+
+# Toggle Monitor
+@app.route("/toggle_monitor/<int:ip>", methods=["POST"])
+def toggle_monitor(ip):
+    cursor.execute("SELECT is_monitored FROM routers WHERE ip = %s", (ip,))
+    result = cursor.fetchone()
+    if result:
+        new_value = not result["is_monitored"]
+        cursor.execute("UPDATE routers SET is_monitored = %s WHERE ip = %s", (new_value, ip))
+        db.commit()
+    return redirect("/")
+
+# RUN
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
