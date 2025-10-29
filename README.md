06c70c55c7b50a369ebbeb3036f8ef56f188cad9
somchoonn <chanokchon.pancome@gmail.com>
2025-10-29 15:58:48 +0700

    Part 3

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # ipa-project
 IPA-Project
 
-# === MySQL ===
+# == ENV ==
 MYSQL_ROOT_PASSWORD=rootpassword
 MYSQL_DATABASE=ipa2025
 MYSQL_USER=root
 MYSQL_PASSWORD=rootpassword
-
-# === Flask ===
 DB_HOST=mysql
 DB_USER=root
 DB_PASSWORD=rootpassword
 DB_NAME=ipa2025
+RABBIT_HOST=rabbitmq
+RABBITMQ_DEFAULT_USER=admin
+RABBITMQ_DEFAULT_PASS=rabbitmq
+DISCORD_WEBHOOK=https://discord.com/api/webhooks/1433012092679622739/tVyRvXVU-YGGHt_wDxCOi-OuetH2Z61nGYRPsW4nMw63k5_B6cRCZYqGucIbJg0bHCGW
 
-# === Ports ===
-WEB_PORT=8080
-MYSQL_PORT=3306
 
 # == Command ==
 docker compose up --build --detach
 docker compose down
+
--- web/app.py
+++ web/app.py
@@ -6,7 +6,6 @@ import time
 app = Flask(__name__)
 
 
-time.sleep(10)
 
 # Connect DB
 db = mysql.connector.connect(
@@ -23,6 +22,50 @@ def index():
     cursor.execute("SELECT * FROM routers")
     routers = cursor.fetchall()
     return render_template("index.html", routers=routers)
+
+# Router Details
+@app.route("/router/<ip>", methods=["GET"])
+def get_router(ip):
+    db.reconnect()
+    cursor = db.cursor(dictionary=True)
+
+    query = """
+        SELECT t1.*
+        FROM interface_status t1
+        INNER JOIN (
+            SELECT interface_name, MAX(last_checked) AS latest
+            FROM interface_status
+            WHERE router_ip = %s
+            GROUP BY interface_name
+        ) t2 ON t1.interface_name = t2.interface_name AND t1.last_checked = t2.latest
+        WHERE t1.router_ip = %s
+        ORDER BY t1.interface_name;
+    """
+
+    cursor.execute(query, (ip, ip))
+    router_details = cursor.fetchall()
+    cursor.close()
+
+    return render_template(
+        "router_detail.html",
+        router_ip=ip,
+        router_details=router_details,
+    )
+# Monitor
+@app.route("/toggle_interface_monitor/<int:id>", methods=["POST"])
+def toggle_interface_monitor(id):
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT is_monitored FROM interface_status WHERE id = %s", (id,))
+    result = cursor.fetchone()
+
+    if result:
+        new_value = not result["is_monitored"]
+        cursor.execute("UPDATE interface_status SET is_monitored = %s WHERE id = %s", (new_value, id))
+        db.commit()
+
+    cursor.close()
+    referer = request.headers.get("Referer", "/")
+    return redirect(referer)
 
 # Add Router
 @app.route("/add", methods=["POST"])
