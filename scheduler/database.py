07f0f3410c71b644c9ec6bdb9f4688590cb4b427
somchoonn <chanokchon.pancome@gmail.com>
2025-10-29 13:52:19 +0700

    Part 2

---
+++ scheduler/database.py
@@ -0,0 +1,26 @@
+import os
+import mysql.connector
+
+def get_router_info():
+    db_host = os.environ.get("DB_HOST")
+    db_user = os.environ.get("DB_USER")
+    db_password = os.environ.get("DB_PASSWORD")
+    db_name = os.environ.get("DB_NAME")
+
+    connection = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM routers")
+    routers = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return routers
+
+if __name__ == "__main__":
+    get_router_info()
---
+++ scheduler/scheduler.py
@@ -0,0 +1,35 @@
+import time
+import json
+import os
+from producer import produce
+from database import get_router_info
+
+def scheduler():
+    INTERVAL = 10.0
+    next_run = time.monotonic()
+    count = 0
+
+    while True:
+        now = time.time()
+        now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
+        ms = int((now % 1) * 1000)
+        now_str_with_ms = f"{now_str}.{ms:03d}"
+        print(f"[{now_str_with_ms}] run #{count}", flush=True)
+
+        try:
+            routers = get_router_info()
+            RABBIT_HOST = os.getenv("RABBIT_HOST")
+            for data in routers:
+                body_bytes = json.dumps(data).encode("utf-8")
+                produce(RABBIT_HOST, body_bytes)
+
+        except Exception as e:
+            print(f"Error: {e}", flush=True)
+            time.sleep(3)
+
+        count += 1
+        next_run += INTERVAL
+        time.sleep(max(0.0, next_run - time.monotonic()))
+
+if __name__ == "__main__":
+    scheduler()
